fb38659cb2e459facbfa4a13a310428bccb724d6
ukassu <152855625+ukassu@users.noreply.github.com>
2023-12-10 11:00:16 +0700

    Update QueryMasingMasing.sql

--- QueryMasingMasing.sql
+++ QueryMasingMasing.sql
@@ -7,7 +7,11 @@ JOIN karyawan ON k_nik = karyawan_k_nik
 WHERE t_tanggal_transaksi BETWEEN '2023-09-25' AND '2023-10-10'
 AND t_metode_pembayaran = 'Kartu debit';
 --2
-
+SELECT ked_nama, COUNT(mn_id) AS jumlah_menu
+FROM kedai
+LEFT JOIN menu ON kedai_ked_id = ked_id AND mn_jenis = 'Makanan'
+GROUP BY ked_nama
+ORDER BY ked_nama;
 
 
 --query
