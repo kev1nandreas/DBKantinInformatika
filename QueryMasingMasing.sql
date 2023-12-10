47281e9102435f49c70f8f292c5c08ed4ede6517
ukassu <152855625+ukassu@users.noreply.github.com>
2023-12-10 10:50:56 +0700

    Update QueryMasingMasing.sql

--- QueryMasingMasing.sql
+++ QueryMasingMasing.sql
@@ -1,6 +1,12 @@
 --querydaffa
-
-
+--1
+SELECT c_nama, k_nama
+FROM customer
+JOIN transaksi ON customer_c_nrp = c_nrp
+JOIN karyawan ON k_nik = karyawan_k_nik
+WHERE t_tanggal_transaksi BETWEEN '2023-09-25' AND '2023-10-10'
+AND t_metode_pembayaran = 'Kartu debit';
+--2
 
 
 
